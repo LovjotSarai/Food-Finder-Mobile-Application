156b95fbd6d5670c0ce1b4e44b1eaa93af9eba92
Lovjot Sarai <123904714+LovjotSarai@users.noreply.github.com>
2023-11-01 14:45:08 -0400

    Create CusineClass.swift

---
+++ CusineClass.swift
@@ -0,0 +1,23 @@
+//
+//  CusineClass.swift
+//  FinalProjectSarai
+//
+//  Created by Lovjot Sarai on 4/28/23.
+//
+
+import Foundation
+import UIKit
+
+class Cusine {
+    var name = ""
+    var description = ""
+    var breakfastItem  = ""
+    var lunchItem = ""
+    var dinnerItem = ""
+    var dessertItem = ""
+    var snackItem = ""
+    var imageURL = ""
+    var mapURL = ""
+    var wikiURL = ""
+    var spiceLevel = ""
+}
